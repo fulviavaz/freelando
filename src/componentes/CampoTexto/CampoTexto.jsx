de6d556419df81d7ab49078e682fd6d61273c838
Fulvia Vaz <fulviavaz.dev@gmail.com>
2023-07-05 18:11:25 -0300

    create Rodape component

---
+++ src/componentes/CampoTexto/CampoTexto.jsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+
+const LabelEstilizada = styled.label`
+  display: block;
+  width: 100%;
+  box-sizing: border-box;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 24px;
+`;
+const InputEstilizado = styled.input`
+  display: block;
+  width: 100%;
+  background-color: ${(props) => props.theme.cores.branco};
+  margin-top: ${(props) => props.theme.espacamentos.xs};
+  border: 1px solid;
+  border-color: ${(props) => props.theme.cores.neutras.a};
+  border-radius: ${(props) => props.theme.espacamentos.s};
+  height: 40px;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 17px;
+`;
+
+export const CampoTexto = ({ titulo }) => {
+  return (
+    <LabelEstilizada>
+      {titulo}
+      <InputEstilizado />
+    </LabelEstilizada>
+  );
+};
--- src/componentes/ProvedorTema/ProvedorTema.jsx
+++ src/componentes/ProvedorTema/ProvedorTema.jsx
@@ -2,7 +2,7 @@ import { ThemeProvider } from "@emotion/react";
 
 const tema = {
   cores: {
-    branco: "",
+    branco: "#FFFFFF",
     atencao: "",
     focus: "",
     primarias: {
@@ -16,7 +16,7 @@ const tema = {
       c: "",
     },
     neutras: {
-      a: "",
+      a: "#373737",
       b: "",
       c: "",
       d: "",
@@ -27,6 +27,7 @@ const tema = {
     },
   },
   espacamentos: {
+    xs: "8px",
     s: "16px",
     l: "32px",
   },
